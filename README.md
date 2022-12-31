4da5a02c2c5fbfa201ac90190301c3f813b594bf
Ann Duong <30392423+thuyanduong@users.noreply.github.com>
2022-12-31 02:23:24 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Problem Set: Structured Problem Solving
+
+### Directions
+1. Run `npm install` to load project dependencies.
+2. Complete coding exercises in the `exercises.js` file. (You will find the questions in `problem-set.md` and unit tests in `exercises.test.js`)
+3. Test your code often by running `npm test`.
+4. Check for style by running `eslint exercises.js` in the terminal. If you do not already have eslint installed, run `npm install -g eslint`.
+
+   **Note:** You are not done with your exercises until all tests pass and there are no eslint errors.
+
+When you are done, push your final commits and submit through Canvas.
---
+++ exercises.js
@@ -0,0 +1,57 @@
+// Question 1
+const isRealPalindrome = () => {
+
+};
+
+// Question 2
+const runningTotal = () => {
+
+};
+
+// Question 3
+const swap = () => {
+
+};
+
+// Question 4
+const wordSizes = () => {
+
+};
+
+// Question 5
+const union = () => {
+
+};
+
+// Question 6
+const firstRecurring = () => {
+
+};
+
+// Question 7
+const showMultiplicativeAverage = () => {
+
+};
+
+// Quetsion 8
+const multiplyList = () => {
+};
+
+// Question 9
+const sequence = () => {
+
+};
+
+
+// The below code connects this JS file to our test file
+module.exports = {
+  isRealPalindrome,
+  runningTotal,
+  swap,
+  wordSizes,
+  union,
+  firstRecurring,
+  showMultiplicativeAverage,
+  multiplyList,
+  sequence,
+};
